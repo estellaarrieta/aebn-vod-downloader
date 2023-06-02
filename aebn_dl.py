5c597e04c4be1dc96581d474d90fd6343d51a108
estellaarrieta <111574945+estellaarrieta@users.noreply.github.com>
2023-06-02 05:51:04 +0300

    Update aebn_dl.py

--- aebn_dl.py
+++ aebn_dl.py
@@ -23,14 +23,19 @@ def modify_manifest(manifest_content, base_url, duration_seconds, target_height)
     # Update the mediaPresentationDuration attribute
     root.set('mediaPresentationDuration', f'PT{duration_seconds}S')
 
-    # Remove streams that do not match the target height
+    # Remove streams that do not match the target height or get the larger height value
+    max_height = 0
     adaptation_sets = root.findall('.//mpd:AdaptationSet', namespaces=ns)
     for adaptation_set in adaptation_sets:
         representations = adaptation_set.findall('mpd:Representation', namespaces=ns)
         for representation in representations:
             height = representation.get('height')
-            if height is not None and int(height) != target_height:
-                adaptation_set.remove(representation)
+            if target_height:
+                if height is not None and int(height) != target_height:
+                    adaptation_set.remove(representation)
+            else:
+                if height is not None and int(height) > max_height:
+                    max_height = int(height)
 
     # Update startNumber attribute in SegmentTemplate elements to 1
     # Update initialization and media paths to absolute URLs
@@ -49,7 +54,10 @@ def modify_manifest(manifest_content, base_url, duration_seconds, target_height)
 
     # Convert the modified XML back to a string
     modified_manifest_content = ET.tostring(root, encoding='utf-8').decode('utf-8')
-    return modified_manifest_content
+    if max_height:
+        return modified_manifest_content, max_height
+    else:
+        return modified_manifest_content, target_height
 
 def download_media(modified_manifest_content, output_path):
     # Create a temporary file to store the modified manifest
@@ -68,11 +76,9 @@ def download_media(modified_manifest_content, output_path):
     temp_manifest_path.unlink()
 
 def movie_scrape(MOVIE_URL):
-    session = requests.Session()
-    session.headers["content-type"] = "application/x-www-form-urlencoded"
-    content = html.fromstring(session.get(MOVIE_URL).content)
+    content = html.fromstring(requests.get(MOVIE_URL).content)
     movie_id = MOVIE_URL.split("/")[5]
-    manifest_url = get_manifest_url(movie_id, session)
+    manifest_url = get_manifest_url(movie_id)
     studio_name = content.xpath('//*[@class="dts-studio-name-wrapper"]/a/text()')[0].strip()
     movie_name = content.xpath('//*[@class="dts-section-page-heading-title"]/h1/text()')[0].strip()
     duration = content.xpath('//*[@class="section-detail-list-item-duration"][2]/text()')[0].strip()
@@ -87,9 +93,11 @@ def get_manifest_content(manifest_url):
     response.raise_for_status()  # Raise an exception for non-2xx status codes
     return response.content
 
-def get_manifest_url(movie_id, session):
+def get_manifest_url(movie_id):
+    headers = {}
+    headers["content-type"] = "application/x-www-form-urlencoded"
     data = f"movieId={movie_id}&isPreview=true&format=DASH"
-    content = session.post("https://straight.aebn.com/straight/deliver", data=data).json()
+    content = requests.post("https://straight.aebn.com/straight/deliver", headers=headers, data=data).json()
     manifest_url = content["url"]
     return manifest_url
 
@@ -97,14 +105,14 @@ def main(MOVIE_URL):
     name, duration_seconds, manifest_url = movie_scrape(MOVIE_URL)
     manifest_content = get_manifest_content(manifest_url)
     base_url = manifest_url.rsplit('/', 1)[0]
-    modified_manifest_content = modify_manifest(manifest_content, base_url, duration_seconds, target_height)
-    name = f"{name} {target_height}p"
+    modified_manifest_content, height = modify_manifest(manifest_content, base_url, duration_seconds, target_height)
+    name = f"{name} {height}p"
     print(name)
     output_path = pathlib.Path(f"{name}.mp4")
     download_media(modified_manifest_content, output_path)
 
 
 if __name__ == "__main__":
-    target_height = 1080
-    MOVIE_URL = "URL" # "https://straight.aebn.com/straight/movies/*"
+    target_height = 1080 #leave blank to let ffmpeg choose the best
+    MOVIE_URL = "" # "https://straight.aebn.com/straight/movies/*"
     main(MOVIE_URL)
