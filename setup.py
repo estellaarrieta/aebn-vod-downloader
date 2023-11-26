006f3b7446827fb616d6b033d82f882bdba89c05
estellaarrieta <111574945+estellaarrieta@users.noreply.github.com>
2023-11-26 18:36:37 +0300

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='aebndl',
-    version='0.3.1',
+    version='0.3.2',
     packages=find_packages(),
     install_requires=required_packages,
     entry_points={
