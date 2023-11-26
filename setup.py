64dbb2663f1139d3e679f8830d40faaf0dce6773
estellaarrieta <111574945+estellaarrieta@users.noreply.github.com>
2023-11-26 10:32:34 +0300

    renamed to aebn-dl

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('requirements.txt') as f:
     required_packages = f.read().splitlines()
 
 setup(
-    name='aebn_dl',
+    name='aebn-dl',
     version='0.3.0',
     packages=find_packages(),
     install_requires=required_packages,
     entry_points={
         'console_scripts': [
-            'aebn_dl = aebn_dl.aebn_dl:main'
+            'aebn-dl = aebn-dl.aebn_dl:main'
         ]
     },
 )
