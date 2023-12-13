2c978b41e080ec563b631f089e524ec88fd71f0c
estellaarrieta <111574945+estellaarrieta@users.noreply.github.com>
2023-12-13 07:44:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'aebndl = aebn_dl.main:main'
+            'console_scripts': ['aebndl = aebn_dl:main'],
         ]
     },
 )
