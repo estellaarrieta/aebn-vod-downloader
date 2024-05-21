d7fdebc1718542c01bde8d11cee78f9606c81870
estellaarrieta <111574945+estellaarrieta@users.noreply.github.com>
2024-05-21 18:23:29 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='aebndl',
-    version='0.4.7',
+    version='0.4.8',
     packages=find_packages(),
     install_requires=[
         'lxml',
