f0ce5c93c29fcfa5130e0634f3a1dce22521259e
estellaarrieta <111574945+estellaarrieta@users.noreply.github.com>
2024-09-01 01:08:40 +0300

    chore: version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='aebndl',
-    version='0.4.8',
+    version='0.4.9',
     packages=find_packages(),
     install_requires=[
         'lxml',
