298f1101e6fe6bf22cba1347b881dac889719e9b
estellaarrieta <111574945+estellaarrieta@users.noreply.github.com>
2025-04-02 13:46:52 +0300

    log threads

--- aebn_dl/downloader.py
+++ aebn_dl/downloader.py
@@ -138,6 +138,7 @@ class Downloader:
         """Log input arguments"""
         self.logger.info(f"Input URL: {self.input_url}")
         self.logger.info(f"Proxy: {self.proxy}")
+        self.logger.info(f"Threads: {self.threads}")
         self.logger.info(f"Output dir: {self.output_dir}")
         self.logger.info(f"Work dir: {self.work_dir}")
         self.logger.info(f"Target stream: {self.target_stream or 'both'}")
